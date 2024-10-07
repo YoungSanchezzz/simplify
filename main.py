7088baf840c025cbdc22c9b145aa1667e927ab81
alexa <alexander.yushko.personal@gmail.com>
2024-10-07 16:32:43 +0300

    Window App

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print(2+3)
-# здарова Саня
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title('blue boat')
+        self.geometry('400x600')
+
+
+
+app = App()
+app.mainloop()
+
